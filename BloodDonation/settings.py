65013e9338af6e5d308b8fb12e0fe02e2d68aa08
Anuj <anuj8ranchi@gmail.com>
2024-04-13 02:58:17 +0530

    static file handling

--- BloodDonation/settings.py
+++ BloodDonation/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2@uvp(%*t*7z0!24trjxrli@--&y0s@za$ad02j!7dpv50i175'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
